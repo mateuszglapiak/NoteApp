5d70fc160d155f0786256ec10ea82f3a1d0c0172
Mateusz Glapiak <mateusz.glapiak@gmail.com>
2023-08-08 12:54:19 +0200

    Create iOS project

---
+++ iOS/NoteApp/NoteApp.swift
@@ -0,0 +1,17 @@
+//
+//  NoteApp.swift
+//  NoteApp
+//
+//  Created by Mateusz on 08/08/2023.
+//
+
+import SwiftUI
+
+@main
+struct NoteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
